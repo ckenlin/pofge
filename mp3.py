8e83f69ead1bc64159380c7488613f58d61235c7
ckenlin <96581920+ckenlin@users.noreply.github.com>
2022-12-25 01:23:22 +0800

    Update mp3.py

--- mp3.py
+++ mp3.py
@@ -42,18 +42,9 @@ if option_year=='2018':
 option_musiclist=st.sidebar.selectbox("選擇類型",type_list)  
 option_music=st.selectbox("選擇音樂",type_list[option_musiclist])
 
-
-
-
-
+  
            
-           
-           
-           
-           
-           
-           
-audio_file = open(option_year+'/'+ption_music+'.mp3', "rb")
+audio_file = open(option_year'/'+ption_music+'.mp3', "rb")
 
 #audio_file = open(option_music+'.mp3', "rb")
 st.audio(audio_file.read())
